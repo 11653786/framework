0a55bcdcdd54f98629cb12f4ced4eff903eeb9f3
11653786@qq.com <yangtao>
2016-04-18 17:10:49 +0800

    6666

--- framework-web/src/main/webapp/view/auth/js/auth.js
+++ framework-web/src/main/webapp/view/auth/js/auth.js
@@ -10,8 +10,32 @@ $(function () {
         {field: 'id', checkbox: true},
         {field: 'authName', title: '权限名称', width: 180},
         {field: 'authUrl', title: '权限url', width: 80},
-        {field: 'authType', title: '权限类型', width: 60, align: 'right'},
-        {field: 'isEnable', title: '是否启用', width: 80}
+        {
+            field: 'authType', title: '权限类型', width: 60, align: 'right', formatter: function (value, row, index) {
+            switch (value) {
+                case 1:
+                    return "按钮"
+                    break;
+                case 2:
+                    return "页面";
+                case 3:
+                    return "菜单";
+                    break;
+            }
+        }
+        },
+        {
+            field: 'isEnable', title: '是否启用', width: 80, formatter: function (value, row, index) {
+            switch (value) {
+                case 0:
+                    return "不可用"
+                    break;
+                case 1:
+                    return "可用";
+                    break;
+            }
+        }
+        }
     ]];
     //初始化树表格
     authdatagrid.init();
@@ -28,9 +52,6 @@ $(function () {
     $("#updateuser").click(function () {
         addOrEditDialog.init('修改', "/api/auth/addOrEdit.do", "update");
     });
-
-
-
 
 
 });
