ab39f8e91b484b8771bb1505eda531cd463027d8
11653786@qq.com <yangtao>
2016-04-27 13:58:57 +0800

    213

--- framework-web/src/main/webapp/static/resource/yt-easyui_valid-1.0.js
+++ framework-web/src/main/webapp/static/resource/yt-easyui_valid-1.0.js
@@ -8,7 +8,16 @@
  });
  //定义和使用方法
  alert($.fn.min(5, 4));*/
+//验证手机号码的正则表达式
+var reg = /^0?1[3|4|5|8][0-9]\d{8}$/;
+function validPhone(phoneNum) {
 
+    if (reg.test(phoneNum)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 /**
  * 自定义的验证
  */
@@ -16,7 +25,7 @@ $.extend($.fn.validatebox.defaults.rules, {
     isPhone: {
         //value，是input的值,param[0]是我传入的验证规则长度
         validator: function (value, param) {
-            return value.length >= param[0];
+            return validPhone(value);
         },
         message: resource.validForm.phoneMsg
     }
