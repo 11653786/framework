d6e31242c9376b55b7f8a6140278213698141681
11653786@qq.com <yangtao>
2016-04-27 09:57:38 +0800

    213

--- framework-web/src/main/java/com/yt/util/SpringUtils.java
+++ framework-web/src/main/java/com/yt/util/SpringUtils.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationContextAware;
 
 /**
  * spring上下文工具类,在applicationContext.xml文件里面注册下这个工具类就可以用了
+ *
  * @author zhangsan
  * @version 1.0
  * @package com.yt.util.sessionutil
@@ -26,4 +27,24 @@ public class SpringUtils implements ApplicationContextAware {
         return applicationContext.getBean(clazz);
     }
 
+    /**
+     * 根据注入名称
+     *
+     * @param ServiceName
+     * @return
+     */
+    public static Object getBean(String ServiceName) {
+        return applicationContext.getBean(ServiceName);
+    }
+
+    /**
+     * 根据注入名称和类名
+     *
+     * @param clazz
+     * @param ServiceName
+     * @return
+     */
+    public static Object getBean(Class clazz, String ServiceName) {
+        return applicationContext.getBean(ServiceName, clazz);
+    }
 }
